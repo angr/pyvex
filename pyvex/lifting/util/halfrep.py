c2caf57caf071fda4f1e2edc069835dc6fe614c3
Eric Gustafson <edg@cs.ucsb.edu>
2018-03-20 15:03:02 -0700

    wip

--- pyvex/lifting/util/halfrep.py
+++ pyvex/lifting/util/halfrep.py
@@ -1,8 +1,9 @@
 
 import logging
 import bitstring
-
 from .vex_helper import *
+from ...expr import *
+from .. import LiftingException
 
 l = logging.getLogger(__name__)
 
@@ -16,22 +17,25 @@ class HalfRep(GymratLifter):
         self.bitstrm = bitstring.ConstBitStream(bytes=self.thedata)
 
     def lift(self, disassemble=False, dump_irsb=False):
-        self.thedata = self.data[:self.max_bytes]
+        irsb_c = IRSBCustomizer(self.irsb)
         if not self.arch.capstone:
             # Well, that won't work
             pass
         else:
-        instr.jump(None, irsb_c.irsb.addr + irsb_c.irsb.size)
+            # Let's give it a try
+            try:
+                cs = self.arch.capstone
+                insn = list(cs.disasm(self.data, self.irsb.addr, count=1))[0]
+                s = "%#08x: %s %s" % (self.irsb.addr, insn.mnemonic, insn.op_str)
+                with open("/tmp/halfrep.log", 'a') as f:
+                    f.write(s + "\n")
+                l.warning("HALFREP: %s" % s)
+                irsb_c.imark(self.irsb.addr, len(insn.bytes), 0)
+            except:
+                l.exception("Error disassembling instruction during halfrep")
+                return None
         irsb_c.irsb.jumpkind = JumpKind.NoDecode
         return self.irsb
-
-    def pp_disas(self):
-        disasstr = ""
-        insts = self.disassemble()
-        for addr, name, args in insts:
-            args_str = ",".join(str(a) for a in args)
-            disasstr += "%0#08x:\t%s %s\n" % (addr, name, args_str)
-        print disasstr
 
     def error(self):
         return self.errors
@@ -40,5 +44,20 @@ class HalfRep(GymratLifter):
         return self.lift(disassemble=True)
 
 
-from ...expr import *
-from .. import LiftingException
+test_location = str(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../../binaries/tests'))
+
+
+def test_full_binary():
+    p = angr.Project(os.path.join(test_location, 'armel', 'RTOSDemo.axf.issue_685'))
+    st = p.factory.call_state(0x000013ce+1)
+    b = st.block().vex
+    simgr = p.factory.simgr(st)
+    simgr.step()
+    nose.tools.assert_equal(b.jumpkind, 'Ijk_Sys_syscall')
+    nose.tools.assert_equal(simgr.active[0].addr, 0x13fb)
+
+if __name__ == '__main__':
+    test_basic()
+    test_embedded()
+    test_full_binary()
+
