5548083c8a3bc69ec20bbb6d9ff75da5b821e2a3
Audrey Dutcher <audrey@rhelmot.io>
2018-05-17 20:59:26 -0700

    Flatten IR

--- pyvex/lifting/zerodivision.py
+++ pyvex/lifting/zerodivision.py
@@ -23,17 +23,17 @@ class ZeroDivisionPostProcessor(Postprocessor):
 
               00 | ------ IMark(0x8000, 3, 0) ------
               01 | t0 = GET:I64(rcx)
-              02 | t1 = GET:I64(rax)
-              03 | t2 = GET:I64(rdx)
-              04 | t3 = 64HLto128(t2,t1)
-              05 | if (CmpEQ(t12,0)) { PUT(pc) = 0x8000; Ijk_SigFPE_IntDiv }
-              05 | t4 = DivModU128to64(t3,t0)
-              06 | t5 = 128to64(t4)
-              07 | PUT(rax) = t5
-              08 | t6 = 128HIto64(t4)
-              09 | PUT(rdx) = t6
+              02 | t4 = GET:I64(rax)
+              03 | t5 = GET:I64(rdx)
+              04 | t3 = 64HLto128(t5,t4)
+              05 | t9 = CmpEQ(t0,0x0000000000000000)
+              06 | if (t9) { PUT(pc) = 0x8000; Ijk_SigFPE_IntDiv }
+              07 | t2 = DivModU128to64(t3,t0)
+              08 | t6 = 128to64(t2)
+              09 | PUT(rax) = t6
+              10 | t7 = 128HIto64(t2)
+              11 | PUT(rdx) = t7
               NEXT: PUT(rip) = 0x0000000000008003; Ijk_Boring
-
     """
 
     def postprocess(self):
@@ -47,8 +47,10 @@ class ZeroDivisionPostProcessor(Postprocessor):
                     s.data.args[1],
                     expr.Const(const.vex_int_class(s.data.args[1].result_size(self.irsb.tyenv))(0))
                 ]
+                cmp_tmp = self.irsb.tyenv.add("Ity_I1")
+                insertions.append((i, stmt.WrTmp(cmp_tmp, expr.Binop('Iop_CmpEQ', cmp_args))))
                 insertions.append((i, stmt.Exit(
-                    expr.Binop('Iop_CmpEQ', cmp_args),
+                    expr.RdTmp(cmp_tmp),
                     const.vex_int_class(self.irsb.arch.bits)(last_ip),
                     'Ijk_SigFPE_IntDiv', self.irsb.offsIP
                 )))
