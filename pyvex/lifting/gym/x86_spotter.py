a439da9465c0ca4a7c1f8281d6466e936a123570
Audrey Dutcher <audrey@rhelmot.io>
2018-10-17 11:31:34 -0700

    lint

--- pyvex/lifting/gym/x86_spotter.py
+++ pyvex/lifting/gym/x86_spotter.py
@@ -1,19 +1,19 @@
 from ..util.lifter_helper import GymratLifter
-from ..util.instr_helper import Instruction, ParseError
+from ..util.instr_helper import Instruction
 from .. import register
 import logging
 
 l = logging.getLogger(__name__)
 
 
-class X86Instruction(Instruction):
+class X86Instruction(Instruction): # pylint: disable=abstract-method
     pass
 
 class Instruction_ENDBR(X86Instruction):
     name = "ENDBR"
     bin_format = '1111001100001111000111101111101b'
 
-    def compute_result(self):
+    def compute_result(self): # pylint: disable=arguments-differ
         # Perhaps, if one wanted to verify ENDBR behavior during compilation
         # Throw some CCall or whatever in here.
         if self.data['b'] == '1':
