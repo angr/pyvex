1571ec8a654b553fcf8847720193d7d880bd4cd6
Audrey Dutcher <audrey@rhelmot.io>
2020-07-18 17:39:57 -0700

    SHIT

--- pyvex/lifting/gym/__init__.py
+++ pyvex/lifting/gym/__init__.py
@@ -1,5 +1,4 @@
 from .arm_spotter import ARMSpotter
-from .x86_spotter import X86Spotter
 from .aarch64_spotter import AARCH64Spotter
 
-__all__ = ('ARMSpotter', 'X86Spotter', 'AARCH64Spotter')
+__all__ = ('ARMSpotter', 'AARCH64Spotter')
