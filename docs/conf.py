0ef3aff710f2ea33c1327177240d945a3d72c6a8
Kevin Phoenix <kevin@kphoenix.us>
2023-08-14 14:53:10 -0700

    Include __init__ member in api docs

--- docs/conf.py
+++ docs/conf.py
@@ -37,6 +37,7 @@ autodoc_default_options = {
     "member-order": "bysource",
     "inherited-members": True,
     "show-inheritance": True,
+    "special-members": "__init__",
     "undoc-members": True,
 }
 autodoc_inherit_docstrings = True
