adaf3facec8e8f2801adf469f3c66c80f6c37513
Kevin Phoenix <kevin@kphoenix.us>
2023-08-30 14:52:24 -0700

    Rewrite test_tmsr and test_tmsr to not use angr

--- tests/test_spotter.py
+++ tests/test_spotter.py
@@ -119,35 +119,23 @@ def test_full_binary():
     assert simgr.active[0].regs.ip_at_syscall.args[0] == 0x13FB
 
 
-@unittest.skipIf(angr is None, "angr required")
 def test_tmrs():
-    test_location = str(os.path.join(os.path.dirname(os.path.realpath(__file__)), "../../binaries/tests"))
-    p = angr.Project(
-        os.path.join(test_location, "armel", "helloworld"),
-        arch="ARMEL",
-        auto_load_libs=False,
-    )
+    arch = archinfo.arch_from_id("ARMEL")
     ins = b"\xef\xf3\x08\x82"
-    b = pyvex.block.IRSB(ins, 1, p.arch)
+    b = pyvex.block.IRSB(ins, 1, arch)
     assert b.jumpkind == "Ijk_Boring"
     assert type(b.statements[1].data) == pyvex.expr.Get
-    assert p.arch.register_names.get(b.statements[1].data.offset, "") == "sp"
+    assert arch.register_names.get(b.statements[1].data.offset, "") == "sp"
     assert type(b.statements[2]) == pyvex.stmt.Put
 
 
-@unittest.skipIf(angr is None, "angr required")
 def test_tmsr():
-    test_location = str(os.path.join(os.path.dirname(os.path.realpath(__file__)), "../../binaries/tests"))
-    p = angr.Project(
-        os.path.join(test_location, "armel", "helloworld"),
-        arch="ARMEL",
-        auto_load_libs=False,
-    )
+    arch = archinfo.arch_from_id("ARMEL")
     inss = b"\x82\xf3\x08\x88"
-    b = pyvex.block.IRSB(inss, 1, p.arch, opt_level=3)
+    b = pyvex.block.IRSB(inss, 1, arch, opt_level=3)
     assert b.jumpkind == "Ijk_Boring"
     assert type(b.statements[1].data) == pyvex.expr.Get
-    assert p.arch.register_names.get(b.statements[1].data.offset, "") == "r2"
+    assert arch.register_names.get(b.statements[1].data.offset, "") == "r2"
     assert type(b.statements[2]) == pyvex.stmt.Put
 
 
