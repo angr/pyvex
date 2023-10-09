64c3ea927f0794e80c0120262cf4cf627e4cf1ec
Bailey Capuano <32396237+capuanob@users.noreply.github.com>
2023-10-09 19:33:07 -0400

    Change OSSFuzz Pip (#358)
    * Moved fuzzing source into pyvex
    
    * Added CI-Fuzz
    
    * Attempt to resolve syntax error in cifuzz
    
    * Force workflow
    
    * Set language to python in cifuzz
    
    * Added a more intelligent catch for value error exceptions
    
    * Resolved inaccurate crashes
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Appeasing the linter a bit
    
    * Resolved another pylint complaint
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Moved atheris requirement to testing in setup.cfg
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * The CI server only has Apple Clang and you can't use atheris on Windows, so make it an optional install
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Moved build-script into pyvex source
    
    * Updated CIFuzz job with a more specific name and to run on pushes to main
    
    * Updated submodule to reflect master
    
    * Specify python3.9 for pip install
    
    * pip3 -> pip
    
    * Change search path for harness resolution
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- fuzzing/build.sh
+++ fuzzing/build.sh
@@ -17,10 +17,10 @@
 
 # Since pyvex requires a specific developer build of archinfo, install it from source
 cd "$SRC"/archinfo
-pip3 install .
+python3.9 -m pip install .
 
 cd "$SRC"/pyvex
-pip3 install .[testing]
+python3.9 -m pip install .[testing]
 
 # Generate a simple binary for the corpus
 echo -ne "start:\n\txor %edi, %edi\nmov \$60, %eax\nsyscall" > /tmp/corpus.s
@@ -28,6 +28,6 @@ clang -Os -s /tmp/corpus.s -nostdlib -nostartfiles -m32 -o corpus
 zip -r "$OUT"/irsb_fuzzer_seed_corpus.zip corpus
 
 # Build fuzzers in $OUT
-for fuzzer in $(find $SRC -name 'fuzzing/*_fuzzer.py'); do
+for fuzzer in $(find $SRC -name '*_fuzzer.py'); do
   compile_python_fuzzer "$fuzzer" --add-binary="pyvex/lib/libpyvex.so:pyvex/lib"
 done
