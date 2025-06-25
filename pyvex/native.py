f1d207ead4e52d90b800077ef46b0419c34353e0
Kevin Phoenix <kevin@kphoenix.us>
2025-06-25 16:37:03 -0700

    Remove problematic line

--- pyvex/native.py
+++ pyvex/native.py
@@ -61,7 +61,6 @@ def _find_c_lib():
         Does the file exist? {os.path.exists(pyvex_path)}
         Does the folder exist? {os.path.isdir(os.path.dirname(pyvex_path))}
         Base contents: {os.listdir(os.path.dirname(__file__))}
-        Folder contents: {(os.path.isdir(os.path.dirname(pyvex_path)) or os.listdir(os.path.dirname(pyvex_path)))}
         """
         )
 
